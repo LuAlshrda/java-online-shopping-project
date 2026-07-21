5b5694db0ecdeeb0cc3fb1e7d1cc676548da3dd0
lujai <lujai@Lujain>
2026-07-21 15:27:22 +0300

    Initial commit - multi module Java project

---
+++ src/ProductManagement/MainProduct.java
@@ -0,0 +1,13 @@
+
+package ProductManagement;
+
+/**
+ *
+ * @author lujain 2241100881
+ */
+public class MainProduct {
+    public static void main(String[] args) {
+        ProductCatalogandInventoryManagement_1 frame = new  ProductCatalogandInventoryManagement_1 ();
+        frame.setVisible(true);
+    }
+}
