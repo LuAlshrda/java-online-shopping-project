36cde3726ac9b64022c3147a95286d87c4026eae
shahadmadouh <shahadmadouhh@gmail.com>
2026-07-21 20:51:57 +0300

    updated

--- src/DispatchManagement/PendingDeliveriesGUI.java
+++ src/DispatchManagement/PendingDeliveriesGUI.java
@@ -161,6 +161,11 @@ public class PendingDeliveriesGUI extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+           new DeliveryDashboardGUI().setVisible(true);
+
+    dispose();
+    
+
         
     }//GEN-LAST:event_jButton1ActionPerformed
      
